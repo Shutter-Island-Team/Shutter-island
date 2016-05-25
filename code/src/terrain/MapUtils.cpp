770b24184fa61c9970749ffc0125282a24520418
Mathieu Stoffel <mathieu.stoffel@ensimag.grenoble-inp.fr>
2016-05-25 15:35:01 +0200

    [Map] Refactoring code
    Some piece of code was not correctly formatted, so I just
    refactored it.

--- code/src/terrain/MapUtils.cpp
+++ code/src/terrain/MapUtils.cpp
@@ -9,13 +9,28 @@
 #include "../../include/terrain/MapUtils.hpp"
 
 
-Biome findClosestBiome(Vertex2D & pos, 
-		       voro::container & container, std::vector<Seed> & seeds){
+Biome findClosestBiome(
+    Vertex2D & pos, 
+	voro::container & container, 
+    std::vector<Seed> & seeds
+){
     double rx, ry, rz;
     int seedID;
-    if (!(container.find_voronoi_cell(pos.first, pos.second, 0.0,
-				      rx, ry, rz, seedID)))
-	exit(EXIT_FAILURE);
+    if (
+            !(
+                container.find_voronoi_cell(
+                    pos.first, 
+                    pos.second, 
+                    0.0,
+                    rx, 
+                    ry, 
+                    rz, 
+                    seedID
+                )
+            )
+    ) {
+        exit(EXIT_FAILURE);
+    }
 
     return seeds[seedID].getBiome();
 }
